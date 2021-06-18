3227c300ea684542403f2bbd34d69acce2750b7e
Jungwon Lee <leejungwon96@gmail.com>
2021-06-18 11:43:40 +0900

    refactor: :hammer: 코드리뷰 반영 (큰 컴포넌트 나누기)

--- FE/issue-tracker/src/Components/Tab/TabModal/Label/LabelAddModal.tsx
+++ FE/issue-tracker/src/Components/Tab/TabModal/Label/LabelAddModal.tsx
@@ -1,4 +1,4 @@
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { AddNewModal as S, TabAssets as Icon } from "../../TabStyles";
 import {
   addNewLabelTitleState,
@@ -9,18 +9,113 @@ import {
 import NewLabelView from "./NewLabelView";
 
 const LabelAddModal = () => {
-  const [newLabelTitleState, setNewLabelTitleState] = useRecoilState(
-    addNewLabelTitleState
+  const newLabelTitleState = useRecoilValue(addNewLabelTitleState);
+
+  return (
+    <S.AddModalDiv isLabel={true}>
+      <S.AddModalTitle>새로운 레이블 추가</S.AddModalTitle>
+      <S.ModalContent>
+        <S.ModalLeft>
+          <NewLabelView />
+        </S.ModalLeft>
+        <S.ModalRight>
+          <LabelTitleDescriptionInput />
+          <S.ChangeColorContainer>
+            <BackgroundChangeContainer />
+            <FontColorChangeContainer />
+          </S.ChangeColorContainer>
+          <S.FinishWriteBtnDiv>
+            <S.FinishWriteBtn disabled={newLabelTitleState === ""}>
+              + 완료
+            </S.FinishWriteBtn>
+          </S.FinishWriteBtnDiv>
+        </S.ModalRight>
+      </S.ModalContent>
+    </S.AddModalDiv>
   );
+};
 
-  const [newLabelDescriptionState, setNewLabelDescriptionState] =
-    useRecoilState(addNewLabelDescriptionState);
-
+const BackgroundChangeContainer = () => {
   const [newLabelBackgroundState, setNewLabelBackgroundState] = useRecoilState(
     addNewLabelBackgroundState
   );
 
+  const handleLabelBackgroundInput = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNewLabelBackgroundState(e.target.value);
+  };
+
+  const handleLabelColorInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewLabelBackgroundState((e.target as HTMLInputElement).value);
+  };
+
+  return (
+    <S.ChangeBackgroundDiv>
+      <S.ColorTitle>배경색상</S.ColorTitle>
+      <S.BackgroundColorContent>
+        <S.Input
+          placeholder="색 입력"
+          value={newLabelBackgroundState}
+          onChange={handleLabelBackgroundInput}
+        />
+      </S.BackgroundColorContent>
+      <S.ColorPickerDiv>
+        <label>
+          <Icon.RefreshIcon />
+          <S.ColorPicker
+            type="color"
+            onChange={handleLabelColorInput}
+            value={newLabelBackgroundState}
+          ></S.ColorPicker>
+        </label>
+      </S.ColorPickerDiv>
+    </S.ChangeBackgroundDiv>
+  );
+};
+
+const FontColorChangeContainer = () => {
   const setNewLabelFontColor = useSetRecoilState(addnewLabelFontColor);
+  const handleLabelBlackWhiteInput = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNewLabelFontColor(e.target.dataset.color);
+  };
+
+  return (
+    <S.ChangeFontColorDiv>
+      <S.ColorTitle>텍스트 색상</S.ColorTitle>
+      <S.FontColorRadioContent>
+        <input
+          id="black"
+          data-color="black"
+          name="labelFontColor"
+          type="radio"
+          onChange={handleLabelBlackWhiteInput}
+        />
+        <label htmlFor="black">어두운 색</label>
+      </S.FontColorRadioContent>
+      <S.FontColorRadioContent>
+        <input
+          id="white"
+          data-color="white"
+          name="labelFontColor"
+          type="radio"
+          onChange={handleLabelBlackWhiteInput}
+        />
+        <label htmlFor="white">밝은 색</label>
+      </S.FontColorRadioContent>
+    </S.ChangeFontColorDiv>
+  );
+};
+
+const LabelTitleDescriptionInput = () => {
+  const [newLabelDescriptionState, setNewLabelDescriptionState] =
+    useRecoilState(addNewLabelDescriptionState);
+
+  const [newLabelTitleState, setNewLabelTitleState] = useRecoilState(
+    addNewLabelTitleState
+  );
 
   const handleLabelTitleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewLabelTitleState(e.target.value);
@@ -32,96 +127,19 @@ const LabelAddModal = () => {
     setNewLabelDescriptionState(e.target.value);
   };
 
-  const handleLabelBackgroundInput = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setNewLabelBackgroundState(e.target.value);
-  };
-
-  const handleLabelBlackWhiteInput = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    //
-    setNewLabelFontColor(e.target.dataset.color);
-  };
-
-  const handleLabelColorInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewLabelBackgroundState((e.target as HTMLInputElement).value);
-  };
-
   return (
-    <S.AddModalDiv isLabel={true}>
-      <S.AddModalTitle>새로운 레이블 추가</S.AddModalTitle>
-      <S.ModalContent>
-        <S.ModalLeft>
-          <NewLabelView />
-        </S.ModalLeft>
-        <S.ModalRight>
-          <S.InputWrapper>
-            <S.Input
-              placeholder="레이블 이름"
-              onChange={handleLabelTitleInput}
-              value={newLabelTitleState}
-            />
-            <S.Input
-              placeholder="설명 (선택)"
-              value={newLabelDescriptionState}
-              onChange={handleLabelDescriptionInput}
-            />
-          </S.InputWrapper>
-          <S.ChangeColorContainer>
-            <S.ChangeBackgroundDiv>
-              <S.ColorTitle>배경색상</S.ColorTitle>
-              <S.BackgroundColorContent>
-                <S.Input
-                  placeholder="색 입력"
-                  value={newLabelBackgroundState}
-                  onChange={handleLabelBackgroundInput}
-                />
-              </S.BackgroundColorContent>
-              <S.ColorPickerDiv>
-                <label>
-                  <Icon.RefreshIcon />
-                  <S.ColorPicker
-                    type="color"
-                    onChange={handleLabelColorInput}
-                    value={newLabelBackgroundState}
-                  ></S.ColorPicker>
-                </label>
-              </S.ColorPickerDiv>
-            </S.ChangeBackgroundDiv>
-            <S.ChangeFontColorDiv>
-              <S.ColorTitle>텍스트 색상</S.ColorTitle>
-              <S.FontColorRadioContent>
-                <input
-                  id="black"
-                  data-color="black"
-                  name="labelFontColor"
-                  type="radio"
-                  onChange={handleLabelBlackWhiteInput}
-                />
-                <label htmlFor="black">어두운 색</label>
-              </S.FontColorRadioContent>
-              <S.FontColorRadioContent>
-                <input
-                  id="white"
-                  data-color="white"
-                  name="labelFontColor"
-                  type="radio"
-                  onChange={handleLabelBlackWhiteInput}
-                />
-                <label htmlFor="white">밝은 색</label>
-              </S.FontColorRadioContent>
-            </S.ChangeFontColorDiv>
-          </S.ChangeColorContainer>
-          <S.FinishWriteBtnDiv>
-            <S.FinishWriteBtn disabled={newLabelTitleState === ""}>
-              + 완료
-            </S.FinishWriteBtn>
-          </S.FinishWriteBtnDiv>
-        </S.ModalRight>
-      </S.ModalContent>
-    </S.AddModalDiv>
+    <S.InputWrapper>
+      <S.Input
+        placeholder="레이블 이름"
+        onChange={handleLabelTitleInput}
+        value={newLabelTitleState}
+      />
+      <S.Input
+        placeholder="설명 (선택)"
+        value={newLabelDescriptionState}
+        onChange={handleLabelDescriptionInput}
+      />
+    </S.InputWrapper>
   );
 };
 
--- FE/issue-tracker/src/Components/Tab/TabModal/Label/LabelEditModal.tsx
+++ FE/issue-tracker/src/Components/Tab/TabModal/Label/LabelEditModal.tsx
@@ -25,29 +25,10 @@ const LabelAddModal = ({ id }: LabelEditProps) => {
           <EditLabelView />
         </S.ModalLeft>
         <S.ModalRight>
-          <S.InputWrapper>
-            <S.Input placeholder="레이블 이름" />
-            <S.Input placeholder="설명 (선택)" />
-          </S.InputWrapper>
+          <LabelEditTitleDescriptionInput />
           <S.ChangeColorContainer>
-            <S.ChangeBackgroundDiv>
-              <S.ColorTitle>배경색상</S.ColorTitle>
-              <S.BackgroundColorContent>
-                <S.Input placeholder="색 입력" />
-              </S.BackgroundColorContent>
-              <Icon.RefreshIcon />
-            </S.ChangeBackgroundDiv>
-            <S.ChangeFontColorDiv>
-              <S.ColorTitle>텍스트 색상</S.ColorTitle>
-              <S.FontColorRadioContent>
-                <input name="labelFontColor" type="radio" />
-                어두운 색
-              </S.FontColorRadioContent>
-              <S.FontColorRadioContent>
-                <input name="labelFontColor" type="radio" />
-                밝은 색
-              </S.FontColorRadioContent>
-            </S.ChangeFontColorDiv>
+            <EditBackgroundContainer />
+            <EditFontColorContainer />
           </S.ChangeColorContainer>
           <S.FinishWriteBtnDiv>
             <S.Canclebtn onClick={handleEditCloseBtnClick}>취소</S.Canclebtn>
@@ -58,6 +39,43 @@ const LabelAddModal = ({ id }: LabelEditProps) => {
         </S.ModalRight>
       </S.ModalContent>
     </S.AddModalDiv>
+  );
+};
+
+const LabelEditTitleDescriptionInput = () => {
+  return (
+    <S.InputWrapper>
+      <S.Input placeholder="레이블 이름" />
+      <S.Input placeholder="설명 (선택)" />
+    </S.InputWrapper>
+  );
+};
+
+const EditBackgroundContainer = () => {
+  return (
+    <S.ChangeBackgroundDiv>
+      <S.ColorTitle>배경색상</S.ColorTitle>
+      <S.BackgroundColorContent>
+        <S.Input placeholder="색 입력" />
+      </S.BackgroundColorContent>
+      <Icon.RefreshIcon />
+    </S.ChangeBackgroundDiv>
+  );
+};
+
+const EditFontColorContainer = () => {
+  return (
+    <S.ChangeFontColorDiv>
+      <S.ColorTitle>텍스트 색상</S.ColorTitle>
+      <S.FontColorRadioContent>
+        <input name="labelFontColor" type="radio" />
+        어두운 색
+      </S.FontColorRadioContent>
+      <S.FontColorRadioContent>
+        <input name="labelFontColor" type="radio" />
+        밝은 색
+      </S.FontColorRadioContent>
+    </S.ChangeFontColorDiv>
   );
 };
 
